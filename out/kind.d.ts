4f99c3cf22607cb4d2cb1839b479eae4ee530b5c
Radu M <root@radu.sh>
2019-08-05 15:02:02 +0300

    Add compiled lib
    Signed-off-by: Radu M <root@radu.sh>

--- out/kind.d.ts
+++ out/kind.d.ts
@@ -1,6 +1,5 @@
 import { Job } from "@brigadecore/brigadier";
 export declare const kindJobImage = "radumatei/golang-kind:1.11-0.4";
-export declare class Kind {
-    job: Job;
-    constructor(job?: Job);
+export declare class KindJob extends Job {
+    constructor(name: string, image?: string);
 }
