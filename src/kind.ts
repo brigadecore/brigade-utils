2840060dc03fa89415d469cc8dc3936f99d6a307
Radu M <root@radu.sh>
2019-08-05 15:01:34 +0300

    Update Kind class to extend Job and update volumes and volume mounts
    Signed-off-by: Radu M <root@radu.sh>

--- src/kind.ts
+++ src/kind.ts
@@ -2,79 +2,73 @@ import { Job } from "@brigadecore/brigadier";
 
 export const kindJobImage = "radumatei/golang-kind:1.11-0.4";
 
-export class Kind {
-    job: Job;
-
-    constructor(job?: Job) {
-        if (job != null) {
-            this.job = job;
-        } else {
-            this.job = new Job("kind", kindJobImage);
+export class KindJob extends Job {
+    constructor(name: string, image?: string) {
+        if (image == undefined) {
+            image = kindJobImage;
         }
+        super(name, image);
 
         // kind needs to run as a privileged pod
-        this.job.privileged = true;
+        this.privileged = true;
 
         // kind needs to add the following volumeMounts to function properly
         // the Brigade project must enable `allowMountHosts`
-        this.job.volumeConfig = [
+        this.volumes = [
             {
-                mount: {
-                    name: "modules",
-                    mountPath: "/lib/modules",
-                    readOnly: true
-                },
-                volume: {
-                    name: "modules",
-                    hostPath: {
-                        path: "/lib/modules",
-                        type: "Directory"
-                    }
+                name: "modules",
+                hostPath: {
+                    path: "/lib/modules",
+                    type: "Directory"
                 }
             },
             {
-                mount: {
-                    name: "cgroup",
-                    mountPath: "/sys/fs/cgroup",
-                },
-                volume: {
-                    name: "cgroup",
-                    hostPath: {
-                        path: "/sys/fs/cgroup",
-                        type: "Directory"
-                    }
+                name: "cgroup",
+                hostPath: {
+                    path: "/sys/fs/cgroup",
+                    type: "Directory"
                 }
             },
             {
-                mount: {
-                    name: "docker-graph-storage",
-                    mountPath: "/var/lib/docker",
-                },
-                volume: {
-                    name: "docker-graph-storage",
-                    emptyDir: {}
-                }
+                name: "docker-graph-storage",
+                emptyDir: {}
+            }
+        ];
+        this.volumeMounts = [
+            {
+                name: "modules",
+                mountPath: "/lib/modules",
+                readOnly: true
+            },
+            {
+                name: "cgroup",
+                mountPath: "/sys/fs/cgroup"
+            },
+            {
+                name: "docker-graph-storage",
+                mountPath: "/var/lib/docker"
             }
         ];
 
         // to add your own tasks to this job, use job.tasks.push()
-        this.job.tasks = [
+        this.tasks = [
             "dockerd-entrypoint.sh &",
             "sleep 20",
             "kind create cluster",
             `export KUBECONFIG="$(kind get kubeconfig-path)"`,
+            "kubectl cluster-info",
 
             // this pod is runing inside a Kubernetes cluster
             // unset environment variables pointing to the host cluster
             // even if the service account is limited, and KUBECONFIG is properly set,
             // some operations might still point to the in-cluster configuration
             "unset $(env | grep KUBERNETES_ | xargs)",
-            "kubectl cluster-info",
 
             // the time needed for the control plane to properly start is around 2 minutes
             //
             // TODO: use a kubectl plugin to wait for all pods of the control plane 
             "sleep 150",
-        ]
+            "kubectl get pods --all-namespaces"
+        ];
     }
 }
--- test/kind.ts
+++ test/kind.ts
@@ -1,37 +1,52 @@
-
 import "mocha";
 import { assert } from "chai";
-import { Kind, kindJobImage } from "../src/kind";
+import { KindJob, kindJobImage } from "../src/kind";
 
 describe("when creating a new Kind job", () => {
-    it("all properties are properly set", () => {
-        let kind = new Kind();
+    it("without an image for the job, the default image is used", () => {
+        let kind = new KindJob("kind");
 
-        assert.equal(kind.job.image, kindJobImage);
-        assert.equal(kind.job.name, "kind");
-        assert.isTrue(kind.job.privileged);
+        assert.equal(kind.name, "kind");
+        assert.equal(kind.image, kindJobImage);
+    });
 
-        assert.equal(kind.job.tasks.length, 7);
-        assert.equal(kind.job.volumeConfig.length, 3);
+    it("and an image is passed, the image is used", () => {
+        let kind = new KindJob("kind", "my-custom-kind-image");
 
-        assert.equal(kind.job.volumeConfig[0]!.mount!.name, "modules");
-        assert.equal(kind.job.volumeConfig[0]!.mount!.mountPath, "/lib/modules");
-        assert.equal(kind.job.volumeConfig[0]!.volume!.name, "modules");
-        assert.equal(kind.job.volumeConfig[0]!.volume!.hostPath!.path, "/lib/modules");
-        assert.equal(kind.job.volumeConfig[0]!.volume!.hostPath!.type, "Directory");
+        assert.equal(kind.name, "kind");
+        assert.equal(kind.image, "my-custom-kind-image");
+    });
 
-        assert.equal(kind.job.volumeConfig[1]!.mount!.name, "cgroup");
-        assert.equal(kind.job.volumeConfig[1]!.mount!.mountPath, "/sys/fs/cgroup");
-        assert.equal(kind.job.volumeConfig[1]!.volume!.name, "cgroup");
-        assert.equal(kind.job.volumeConfig[1]!.volume!.hostPath!.path, "/sys/fs/cgroup");
-        assert.equal(kind.job.volumeConfig[1]!.volume!.hostPath!.type, "Directory");
+    it("job is privilged, with right number of pre-defined tasks", () => {
+        let kind = new KindJob("kind");
 
-        assert.equal(kind.job.volumeConfig[2]!.mount!.name, "docker-graph-storage");
-        assert.equal(kind.job.volumeConfig[2]!.mount!.mountPath, "/var/lib/docker");
-        assert.equal(kind.job.volumeConfig[2]!.volume!.name, "docker-graph-storage");
-        assert.deepEqual(kind.job.volumeConfig[2]!.volume!.emptyDir, {});
+        assert.isTrue(kind.privileged);
+        assert.equal(kind.tasks.length, 8);
+    });
 
-        kind.job.tasks.push("new-task");
-        assert.equal(kind.job.tasks.length, 8);
+    it("all volumes are properly set", () => {
+        let kind = new KindJob("kind");
+        assert.equal(kind.volumeMounts.length, 3);
+
+        assert.equal(kind.volumes[0].name, "modules");
+        assert.equal(kind.volumes[0].hostPath!.path, "/lib/modules");
+        assert.equal(kind.volumes[0].hostPath!.type, "Directory");
+        assert.equal(kind.volumes[1].name, "cgroup");
+        assert.equal(kind.volumes[1].hostPath!.path, "/sys/fs/cgroup");
+        assert.equal(kind.volumes[1].hostPath!.type, "Directory");
+        assert.equal(kind.volumes[2].name, "docker-graph-storage");
+        assert.deepEqual(kind.volumes[2].emptyDir!, {});
+    });
+
+    it("all volume mounts are properly set", () => {
+        let kind = new KindJob("kind");
+
+        assert.equal(kind.volumeMounts.length, 3);
+        assert.equal(kind.volumeMounts[0].name, "modules");
+        assert.equal(kind.volumeMounts[0].mountPath, "/lib/modules");
+        assert.equal(kind.volumeMounts[1].name, "cgroup");
+        assert.equal(kind.volumeMounts[1].mountPath, "/sys/fs/cgroup");
+        assert.equal(kind.volumeMounts[2].name, "docker-graph-storage");
+        assert.equal(kind.volumeMounts[2].mountPath, "/var/lib/docker");
     });
 });
